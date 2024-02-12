d452fbcc312132e05c4522038b8dd6cf1a4c0649
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-02-13 00:40:27 +0600

    calculate the triangle area

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-scr
+function getTriangleInfo() {
+    const triangleBase = getValuesById('triangle-base');
+    const triangleHeight = getValuesById('triangle-height');
+    const area = 0.5 * triangleBase * triangleHeight;
+        
+}
+
+//* getting the values with this function
+function getValuesById(value) {
+    return parseFloat(document.getElementById(value).value);
+}
