716173d61d2654fde187e374e3d781b13a4cb198
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-02-13 01:44:35 +0600

    added triangle functionality

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ function getTriangleInfo() {
     const triangleBase = getValuesById('triangle-base');
     const triangleHeight = getValuesById('triangle-height');
     const area = 0.5 * triangleBase * triangleHeight;
-        
+    const calculation = document.createElement('p');
+    calculation.innerText = "Triangle: " + area;
+    const result =  document.getElementById('calculate-area').appendChild(calculation);
+    result.style.marginTop = '20px';
+    result.style.marginLeft = '20px'
+    
 }
 
 //* getting the values with this function
