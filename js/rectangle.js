9af590a3d6692aa702c10870ec376fb761f2e05b
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-02-13 12:35:32 +0600

    made code reusable and efficient

--- js/rectangle.js
+++ js/rectangle.js
@@ -2,9 +2,5 @@ function getRectangleInfo() {
     const rectWidth = getValuesById('rectangle-width');
     const rectHeight = getValuesById('rectangle-height');
     const rectArea = rectWidth * rectHeight;
-    const calculation = document.createElement('p');
-    calculation.innerText = "Rectangle: " + rectArea;
-    const result = document.getElementById('calculate-area').appendChild(calculation);
-    result.style.marginTop = '20px';
-    result.style.marginLeft = '20px';
+    calculation('calculate-area', rectArea, 'Rectangle');
 }
--- js/triangle.js
+++ js/triangle.js
@@ -1,13 +1,17 @@
 function getTriangleInfo() {
-    const triangleBase = getValuesById('triangle-base');
-    const triangleHeight = getValuesById('triangle-height');
+    const triangleBase = parseFloat(getValuesById('triangle-base'));
+    const triangleHeight = parseFloat(getValuesById('triangle-height'));
     const area = 0.5 * triangleBase * triangleHeight;
+    calculation('calculate-area', area, 'Triangle'); // Pass the area to the calculation function
+}
+
+//* printing the result in calculate area section
+function calculation(calculateArea, area, shape) {
     const calculation = document.createElement('p');
-    calculation.innerText = "Triangle: " + area;
-    const result =  document.getElementById('calculate-area').appendChild(calculation);
+    calculation.innerText = shape + " Area: " + area;
+    const result = document.getElementById(calculateArea).appendChild(calculation);
     result.style.marginTop = '20px';
-    result.style.marginLeft = '20px'
-    
+    result.style.marginLeft = '20px';
 }
 
 //* getting the values with this function
